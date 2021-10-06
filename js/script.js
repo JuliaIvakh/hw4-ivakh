59248e9d4348e83507e5bc7caac94ecd402d1e03
JuliaIvakh <85705848+JuliaIvakh@users.noreply.github.com>
2021-10-06 22:40:24 +0300

    changed code

--- js/script.js
+++ js/script.js
@@ -1,17 +1,22 @@
-let operation = prompt("Which operation: add, sub, mult, div ?");
-const firstNum = prompt("First number");
-const secondNum = prompt("Second number");
+const operation = prompt("Which operation: add, sub, mult, div ?");
+const firstNum = +prompt("First number");
+const secondNum = +prompt("Second number");
 
 let result;
-if (operation == "add")
-  (result = `${+firstNum + +secondNum}`),
-    alert(`${firstNum} + ${secondNum} = ${result}`);
-else if (operation == "sub")
-  (result = `${+firstNum - +secondNum}`),
-    alert(`${firstNum} - ${secondNum} = ${result}`);
-else if (operation == "mult")
-  (result = `${+firstNum * +secondNum}`),
-    alert(`${firstNum} * ${secondNum} = ${result}`);
-else if (operation == "div")
-  (result = `${+firstNum / +secondNum}`),
-    alert(`${firstNum} / ${secondNum} = ${result}`);
+if (!isNaN(firstNum) && !isNaN(secondNum)) {
+  if (operation === "add") {
+    (result = firstNum + secondNum),
+      alert(`${firstNum} + ${secondNum} = ${result}`);
+  } else if (operation === "sub") {
+    (result = firstNum - secondNum),
+      alert(`${firstNum} - ${secondNum} = ${result}`);
+  } else if (operation === "mult") {
+    (result = firstNum * secondNum),
+      alert(`${firstNum} * ${secondNum} = ${result}`);
+  } else if (operation === "div") {
+    (result = firstNum / secondNum),
+      alert(`${firstNum} / ${secondNum} = ${result}`);
+  }
+} else {
+  alert("Write NUMBERS!");
+}
